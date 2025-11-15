f0a29a71d5809769ebbca04419001fd31dfaf2c6
tpxxwz <tpxxwz@gmail.com>
2025-11-15 23:30:48 +0900

    init

--- src/_06_conversion/mod.rs
+++ src/_06_conversion/mod.rs
@@ -5,3 +5,4 @@ pub mod _01_from_and_into_12_into;
 pub mod _01_from_and_into_13_exchange;
 pub mod _02_try_from_and_try_into;
 pub mod _03_to_and_from_strings_01_to_string;
+pub mod _03_to_and_from_strings_02_from_string;

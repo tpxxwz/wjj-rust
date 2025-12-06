7d4d6a989ff8f55f97e51e7a5d8e21ac7dbea404
tpxxwz <tpxxwz@gmail.com>
2025-12-07 04:01:41 +0900

    notify

--- vendor/async/vendor_async_tokio/src/_07_async_in_depth.rs
+++ vendor/async/vendor_async_tokio/src/_07_async_in_depth.rs
@@ -1,8 +1,9 @@
+use futures::executor::block_on;
 use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex, mpsc};
-use std::task::{Context, Poll};
+use std::task::{Context, Poll, Waker};
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -88,6 +89,7 @@ impl Future for MainFuture {
 
 use futures::task;
 use futures::task::ArcWake;
+use tokio::sync::Notify;
 
 // 用VecDeque Task 和 for循环来实现 任务做完的异步
 struct MiniTokio {
@@ -171,6 +173,75 @@ fn mini_tokio_test() {
     });
 
     mini_tokio.run();
+}
+
+struct Delay1 {
+    when: Instant,
+    // This is Some when we have spawned a thread, and None otherwise.
+    waker: Option<Arc<Mutex<Waker>>>,
+}
+
+impl Future for Delay1 {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        // Check the current instant. If the duration has elapsed, then
+        // this future has completed so we return `Poll::Ready`.
+        if Instant::now() >= self.when {
+            return Poll::Ready(());
+        }
+
+        // The duration has not elapsed. If this is the first time the future
+        // is called, spawn the timer thread. If the timer thread is already
+        // running, ensure the stored `Waker` matches the current task's waker.
+        if let Some(waker) = &self.waker {
+            let mut waker = waker.lock().unwrap();
+
+            // Check if the stored waker matches the current task's waker.
+            // This is necessary as the `Delay` future instance may move to
+            // a different task between calls to `poll`. If this happens, the
+            // waker contained by the given `Context` will differ and we
+            // must update our stored waker to reflect this change.
+            if !waker.will_wake(cx.waker()) {
+                *waker = cx.waker().clone();
+            }
+        } else {
+            let when = self.when;
+            let waker = Arc::new(Mutex::new(cx.waker().clone()));
+            self.waker = Some(waker.clone());
+
+            // This is the first time `poll` is called, spawn the timer thread.
+            thread::spawn(move || {
+                let now = Instant::now();
+
+                if now < when {
+                    thread::sleep(when - now);
+                }
+
+                // The duration has elapsed. Notify the caller by invoking
+                // the waker.
+                let waker = waker.lock().unwrap();
+                waker.wake_by_ref();
+            });
+        }
+
+        // By now, the waker is stored and the timer thread is started.
+        // The duration has not elapsed (recall that we checked for this
+        // first thing), ergo the future has not completed so we must
+        // return `Poll::Pending`.
+        //
+        // The `Future` trait contract requires that when `Pending` is
+        // returned, the future ensures that the given waker is signalled
+        // once the future should be polled again. In our case, by
+        // returning `Pending` here, we are promising that we will
+        // invoke the given waker included in the `Context` argument
+        // once the requested duration has elapsed. We ensure this by
+        // spawning the timer thread above.
+        //
+        // If we forget to invoke the waker, the task will hang
+        // indefinitely.
+        Poll::Pending
+    }
 }
 
 struct MiniTokio1 {
@@ -320,11 +391,43 @@ fn mini_tokio1_test() {
 
     mini_tokio.spawn(async {
         let when = Instant::now() + Duration::from_millis(100);
-        let future = Delay { when };
-
+        let future = Delay1 { when, waker: None };
         let out = future.await;
-        assert_eq!(out, "done");
+        assert_eq!(out, ());
+    });
+    mini_tokio.run();
+}
+
+async fn delay(dur: Duration) -> &'static str {
+    let when = Instant::now() + dur;
+    // This utility provides a basic task notification mechanism.
+    // It handles the details of wakers,
+    // including making sure that the recorded waker matches the current task.
+    let notify = Arc::new(Notify::new());
+    let notify_clone = notify.clone();
+
+    thread::spawn(move || {
+        let now = Instant::now();
+
+        if now < when {
+            thread::sleep(when - now);
+        }
+
+        notify_clone.notify_one();
     });
 
+    notify.notified().await;
+    println!("delay done");
+    "done"
+}
+
+#[test]
+fn notify_test() {
+    let mut mini_tokio = MiniTokio1::new();
+
+    mini_tokio.spawn(async {
+        let out = delay(Duration::from_secs(1)).await;
+        assert_eq!(out, "done");
+    });
     mini_tokio.run();
 }

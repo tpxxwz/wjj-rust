400dd6d398683a7ca7c99dcb59443945bab36e7c
wjj <tpxxwz@gmail.com>
2025-12-22 02:31:00 +0900

    update

--- wjj_lib/src/core.rs
+++ wjj_lib/src/core.rs
@@ -1,42 +1,6 @@
-use anyhow::{Context, Error, Result};
-use std::fmt::{Display, Formatter};
-// use std::fmt::Display;
-//
-// pub trait Exception {}
-// pub struct SysErr {}
-// pub struct BizErr {}
-// pub enum WjjError {
-//     SystemErr(dyn Exception),
-//     BusinessErr(dyn Exception),
-// }
-//
-// pub trait ContextExt<T, E> {
-//     fn context_obj<C>(self, obj: C) -> Result<T, Error>
-//     where
-//         C: Display + Send + Sync + 'static;
-// }
-//
-// impl<T, E> ContextExt<T, E> for Result<T, E>
-// where
-//     E: std::error::Error + Send + Sync + 'static,
-// {
-//     fn context_obj<C>(self, obj: C) -> Result<T, Error>
-//     where
-//         C: Display + Send + Sync + 'static,
-//     {
-//         self.with_context(|| self.with_context(|| obj) // 自动转换成闭包
-//     }
-// }
-
-// format_args!("Hello {}, age {}", "Rust", 18)
-
-pub struct Exception {}
-
-impl Display for Exception {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
+use anyhow::{Error, Result};
+use std::fmt;
+use std::fmt::Arguments;
 
 pub trait ErrorCode {
     fn error_code(&self) -> &'static str;
@@ -48,34 +12,48 @@ pub trait RawError: ErrorCode {
 
 pub trait FmtError: ErrorCode {
     fn error_fmt_msg(&self) -> &'static str;
-    fn error_fmt_desc(&self) -> &'static str;
 }
 
-pub trait ContextExt<T, E>: Context<T, E> {
-    fn with_raw_error<C>(self, context: C) -> Result<T>
-    where
-        C: RawError;
-    fn with_fmt_error<C>(self, context: C) -> Result<T>
-    where
-        C: Display + Send + Sync + 'static;
+// 自定义错误类型
+#[derive(Debug)]
+struct MyCustomError {
+    code: &'static str,
+    message: String,
 }
 
-impl<T, E> ContextExt<T, E> for Result<T, E>
-where
-    Result<T, E>: Context<T, E>,
-    E: Display + Send + Sync + 'static,
-{
-    fn with_raw_error<C>(self, context: C) -> Result<T>
-    where
-        C: RawError,
-    {
-        self.with_context(|| Exception)
+// 实现 Display
+impl fmt::Display for MyCustomError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error {}: {}", self.code, self.message)
     }
-    fn with_fmt_error<C>(self, context: C) -> Result<T>
-    where
-        C: Display + Send + Sync + 'static,
-    {
-        println!("Logging: {}", context);
-        self.context(context)
+}
+
+impl MyCustomError {
+    // 从 RawError 构造
+    fn from_raw<E: RawError>(err: E) -> Self {
+        MyCustomError {
+            code: err.error_code(),
+            message: err.error_msg().to_string(),
+        }
     }
+
+    // 从 FmtError + Arguments 构造
+    // fn from_fmt<E: FmtError>(err: E, args: Arguments) -> Self {
+    //
+    //     MyCustomError {
+    //         code: err.error_code(),
+    //         message: format!(err.error_fmt_msg(), args),
+    //     }
+    // }
+}
+
+// 实现 std::error::Error
+impl std::error::Error for MyCustomError {}
+
+// 模拟一个返回 anyhow::Error 的函数
+fn do_something() -> Result<()> {
+    Err(Error::new(MyCustomError {
+        code: "404",
+        message: "Resource not found".to_string(),
+    }))
 }
